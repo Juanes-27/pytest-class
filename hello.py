8308ac06fc0d41727741840fd8d94ff48f24cb84
Juan Esteban Hernandez Alvarez <42393221+Juanes-27@users.noreply.github.com>
2026-03-15 06:18:15 +0000

    adding initial skeleton for python proyect

---
+++ hello.py
@@ -0,0 +1,14 @@
+def more_hello():
+    return "hi"
+
+
+def more_goodbye():
+    return "bye"
+
+
+# make nonsense codes that its correct generate a warning
+# var = 1
+# var = var
+
+# Bad syntax
+# foo =
