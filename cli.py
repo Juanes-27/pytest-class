34498d5fed3632383c687cfea2da4b9272708b9e
Juanes <juaneshernandez2000@gmail.com>
2026-04-01 12:25:57 -0600

    fixing makefile lint to catch up all .py files and also creating cli tool using click

---
+++ cli.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env
+import click
+from mlib.mchange import change
+
+
+@click.command()
+@click.option(
+    "--amount",
+    prompt="Amount: ",
+    help="Creates change for dollar and cents value:  i.e. 1.34",
+)
+def make_change(amount):
+    """Gives Correct Change"""
+
+    result = change(float(amount))
+    click.echo(click.style(f"Change for {amount}:", fg="red"))
+    for correct_change in result:
+        for num, coin in correct_change.items():
+            click.echo(click.style(f"{coin}: {num}", fg="green"))
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    make_change()
